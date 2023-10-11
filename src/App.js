f59bdc9d938308fd0dc94da24df651edcbc12110
Shreyas <sadavarte491@gmail.com>
2023-10-12 01:15:47 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
 
 import React,{ useState }from 'react';
 import axios from 'axios';
+import dateFormat from 'dateformat';
 import './index.css';
 
 function App() {
   const [data, setData] = useState({})
+  const [error, setError] = useState('')
   const [location,setLocation] = useState('')
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=4232a7a1daf9d907ce115114831204ea`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=4232a7a1daf9d907ce115114831204ea`
   const searchLocation=(event) =>{
     if (event.key === 'Enter'){
       axios.get(url).then((response)=>{
         setData(response.data)
       console.log(response.data)
     })
+    .catch (err => {
+      if (err.response.status==404){
+       setError('Invalid city name')
+      } else {
+       setError('')
+      }
+    console.log(err)
+   });
     setLocation('')
+    setError('')
+    setData('')
     }
   }
+
+   const renderDate = () =>{
+    let now = new Date();
+    return dateFormat(now,"dddd,mmmm dS, h:MM TT")
+
+   }
  
   return (
-    <div className="app">
+    <div className={(data.main)? ((data.main.temp>30)? 'app': 'app warm'):'app'}>
       <div className='search'>
         <input value={location}
         onChange={event=>setLocation(event.target.value)}
         onKeyPress = {searchLocation}
         placeholder='Enter location'
-        type='text'/>
+        type='text' required/>
       </div>
+    <div className='error'>
+     <p>{error}</p>
+       </div>
+       
       <div className="container">
       <div className="top">
       <div className='location'>
         <p>{data.name}</p>
+        
       </div>
      <div className='temp'>
-      {data.main ? <h1>{data.main.temp.toFixed()}{'\u00b0'}F</h1> :null }
+      {data.main ? <h1>{data.main.temp.toFixed()}{'\u00b0'}c</h1> :null }
+      {data.main ?<p>{renderDate()}</p> : null}
+      {data.weather ?<img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="Image" className='weathicon'/> :null }
       </div>
       <div className='description'>
         {data.weather ? <p>{data.weather[0].main}</p> : null }
@@ -54,6 +79,11 @@ function App() {
         <p>Wind</p>
       </div>
     </div>
+}
+
+{!data.weather && <div className='bottom'>
+  <h3>NO data found</h3>
+  </div>
 }
       </div>
     </div>
